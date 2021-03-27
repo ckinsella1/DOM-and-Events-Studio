bbf07c6fe6ccf2a82ecb3be70a387baa6ad324af
ckinsella1 <78434004+ckinsella1@users.noreply.github.com>
2021-03-27 16:13:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,79 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function init () {
+    const landing = document.getElementById("landing");
+    const missionAbort = document.getElementById("missionAbort");
+    const takeoff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus"); 
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const Up = document.getElementById("Up");
+    const Down = document.getElementById("Down");
+    const Left = document.getElementById("Left");
+    const Right = document.getElementById("Right");
+    const rocket = document.getElementById("rocket");
+    rocket.style.position= 'relative';
+    rocket.style.left = '0px';
+    rocket.style.top = '0px';
+}
+
+function takeOffWindow() {
+    let response = confirm("Confirm that the shuttle is ready for takeoff.");
+  if (response) {
+    var i=0;
+    flightStatus.innerHTML = "";
+    flightStatus.innerHTML += "Shuttle in flight."; 
+    shuttleBackground.style.backgroundColor='blue';
+    i++;       
+    spaceShuttleHeight.innerHTML=(i*10000);
+    } else {
+    console.log("");
+    }
+}
+
+function landButton() {
+    let response = confirm("The shuttle is landing. Landing gear engaged.");
+  if (response) {
+    flightStatus.innerHTML = "";
+    flightStatus.innerHTML += "The shuttle has landed."; 
+    shuttleBackground.style.backgroundColor='green';
+    spaceShuttleHeight.innerHTML = 0;
+    } else {
+    console.log("");
+    }
+}
+
+
+function abortMissionButton() {
+    let response = confirm("Confirm that you want to abort the mission.");
+  if (response) {
+    flightStatus.innerHTML = "";
+    flightStatus.innerHTML += "Mission aborted."; 
+    shuttleBackground.style.backgroundColor='green';
+    spaceShuttleHeight.innerHTML = 0;
+    } else {
+    console.log("");
+    }
+}
+
+var i=0; 
+function upButton() {
+    i++;       
+    spaceShuttleHeight.innerHTML=(i*10000);
+    rocket.style.top = parseInt(rocket.style.top) - 10 + 'px';
+}
+
+function downButton() {
+    if (i > 0)   { 
+    --i;       
+    spaceShuttleHeight.innerHTML=(i*10000);
+    }
+    rocket.style.top = parseInt(rocket.style.top) + 10 + 'px';
+}
+
+function rightButton() {
+    rocket.style.left = parseInt(rocket.style.left) + 10 + 'px';
+}
+
+function leftButton() {
+    rocket.style.left = parseInt(rocket.style.left) - 10 + 'px';
+}
+window.addEventListener("load", init);
